aa83354866f8e4929f8ef5a09141034b27b108a1
Adam Rush <adamrushy@gmail.com>
2024-09-17 10:20:16 +0100

    Adding initial project, get ready to relay 🏆

---
+++ Code Relay/Code Relay/Code_RelayApp.swift
@@ -0,0 +1,17 @@
+//
+//  Code_RelayApp.swift
+//  Code Relay
+//
+//  Created by Adam Rush on 17/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Code_RelayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Code Relay/Code Relay/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Code Relay
+//
+//  Created by Adam Rush on 17/09/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
