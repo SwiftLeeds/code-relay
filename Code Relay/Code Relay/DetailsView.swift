33ffe9028770595abfb87ebd4813b8d3d915f86a
Matt Heaney <Matt@hackedrealitystudio.com>
2024-10-08 16:56:17 +0100

    Added the base of a map view to the details view

--- Code Relay/Code Relay/DetailsView.swift
+++ Code Relay/Code Relay/DetailsView.swift
@@ -6,15 +6,25 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct DetailsView: View {
    
-    let location: Location
+    //To do - link this up to actual coordinates!
+    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 0, longitude: 0), 
+                                                   span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
+
+    @State var location: Location
     
     var body: some View {
         VStack {
+            Map(initialPosition: .region(region))
+                .frame(height: 400)
+                .frame(maxWidth: .infinity)               
+            
             Text(location.title).fontWeight(.bold)
             Text(location.details)
         }
+        .frame(maxHeight: .infinity, alignment: .top)
     } 
 }
