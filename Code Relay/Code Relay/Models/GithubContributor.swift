120da20a846c6f3c504178fcf0be023fdd5a5312
Gerry Wood <gerald.wood@sumup.com>
2024-10-08 23:45:53 +0100

    Let's give ourselves some credit

---
+++ Code Relay/Code Relay/Models/GithubContributor.swift
@@ -0,0 +1,29 @@
+//
+//  Contributor.swift
+//  Code Relay
+//
+//  Created by Gerald.Wood on 10/8/24.
+//
+
+import Foundation
+
+struct Contributor: Identifiable {
+    var id = UUID()
+    let login: String
+    let avatarUrl: String
+    let contributions: Int
+}
+
+extension Contributor {
+    struct CodingData: Decodable {
+        let login: String
+        let avatarUrl: String
+        let contributions: Int
+        
+        var contributor: Contributor {
+            Contributor(login: login,
+                        avatarUrl: avatarUrl,
+                        contributions: contributions)
+        }
+    }
+}
