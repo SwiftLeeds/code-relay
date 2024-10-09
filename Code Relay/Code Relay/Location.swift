9a4d2fabfae0b7b532e9046aaebc4eb209690fc5
Arnaud Wurmel <arnaud.wurmel1@gmail.com>
2024-10-09 13:29:57 +0100

    :fire: Remove initial idea setup

--- Code Relay/Code Relay/Location.swift
+++ Code Relay/Code Relay/Location.swift
@@ -10,7 +10,6 @@ struct Location: Identifiable {
     
     let title: String
     let details: String
-    let address: String
     let coordinate: CLLocationCoordinate2D?
     let phoneNumber: String?
     var id: String { title }
