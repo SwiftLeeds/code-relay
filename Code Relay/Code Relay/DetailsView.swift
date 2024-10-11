48fdb61b02fa09ce95219f4322b1abedb1646638
Gerry Wood <gerald.wood@sumup.com>
2024-10-11 20:56:14 +0100

    Most Everywhere is Somewhere

--- Code Relay/Code Relay/DetailsView.swift
+++ Code Relay/Code Relay/DetailsView.swift
@@ -12,7 +12,16 @@ struct DetailsView: View {
    
     @State private var showCheekyPrawn = false
     @State private var showConfetti = false
+    @State private var region: MKCoordinateRegion
     @State var location: Location
+
+    init(location: Location) {
+        self.location = location
+        self.region = MKCoordinateRegion(
+                center: location.coordinate ?? CLLocationCoordinate2D(latitude: 53.8059743, 
+                                                                      longitude: -1.6181322),
+                span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+    }
     
     var body: some View {
         ZStack {
@@ -58,4 +67,15 @@ struct DetailsView: View {
         showCheekyPrawn.toggle()
         showConfetti = showCheekyPrawn
     }
+}
+
+#Preview {
+    DetailsView(
+        location: Location(
+            title: "A Place",
+            details: "The Place",
+            coordinate: CLLocationCoordinate2D(latitude: 53.7981911, longitude: -1.5376449),
+            phoneNumber: nil
+        )
+    )
 }
--- Code Relay/Code Relay/ViewModel.swift
+++ Code Relay/Code Relay/ViewModel.swift
@@ -14,14 +14,37 @@ final class ViewModel: ObservableObject {
             Location(
                 title: "Leeds Playhouse",
                 details: "Good Conference",
-                coordinate: CLLocationCoordinate2D(latitude: 53.7982, longitude: -1.5351),
+                coordinate: CLLocationCoordinate2D(latitude: 53.7981911, longitude: -1.5376449),
                 phoneNumber: "0044 113 213 7700"
             ),
-            Location(title: "Leeds Art Gallery", details: "Good Art", coordinate: nil, phoneNumber: "0044 113 378 5350"),
-            Location(title: "Victoria Leeds", details: "Something good", coordinate: nil, phoneNumber: "0044 113 245 5333"),
-            Location(title: "Royal Armouries", details: "Good Armor", coordinate: nil, phoneNumber: "0044 113 220 1916"),
-            Location(title: "Brew Society", details: "Good Beer", coordinate: nil, phoneNumber: nil),
-            Location(title: "Brew Dog", details: "Good Dog Beer", coordinate: nil, phoneNumber: "0044 113 391 2950"),
+            Location(
+                title: "Leeds Art Gallery",
+                details: "Good Art",
+                coordinate: CLLocationCoordinate2D(latitude: 53.7999507, longitude: -1.5508274),
+                phoneNumber: "0044 113 378 5350"
+            ),
+            Location(
+                title: "Victoria Leeds",
+                details: "Something good",
+                coordinate: CLLocationCoordinate2D(latitude: 53.798151, longitude: -1.5417475),
+                phoneNumber: "0044 113 245 5333"
+            ),
+            Location(
+                title: "Royal Armouries",
+                details: "Good Armor",
+                coordinate: CLLocationCoordinate2D(latitude: 53.7917894, longitude: -1.5348083),
+                phoneNumber: "0044 113 220 1916"
+            ),
+            Location(title: "Brew Society", 
+                     details: "Good Beer", 
+                     coordinate: CLLocationCoordinate2D(latitude: 53.7956807, longitude: -1.5528864), 
+                     phoneNumber: nil),
+            Location(
+                title: "Brew Dog",
+                details: "Good Dog Beer",
+                coordinate: CLLocationCoordinate2D(latitude: 53.8010952, longitude: -1.5413394),
+                phoneNumber: "0044 113 391 2950"
+            ),
             Location(title: "NQ64 Arcade Bar", details: "Retro Gaming Bar", coordinate: nil, phoneNumber: "0044 113 245 5333"),
             Location(title: "K-Cube", details: "Karaoke all night long", coordinate: nil, phoneNumber: "0044 113 322 6625"),
         ]
